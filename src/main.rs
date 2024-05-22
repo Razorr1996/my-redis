7358b506c54b745413a6bb5f6c64ef8121b18eee
Artem Basalaev <basa62.1996@gmail.com>
2024-05-22 23:42:07 +0200

    Moving hello-redis to examples

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,1 @@
-use mini_redis::{client, Result as MiniResult};
-
-#[tokio::main]
-async fn main() -> MiniResult<()> {
-    // Open a connection to the mini-redis address.
-    let mut client = client::connect("localhost:6379").await?;
-
-    // Set the key "hello" with value "world"
-    client.set("hello", "world".into()).await?;
-
-    // Get key "hello"
-    let result = client.get("hello").await?;
-
-    println!("get value from the server; result={:?}", result);
-
-    Ok(())
-}
+fn main() {}
