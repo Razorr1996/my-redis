98f9e8462927f606f32f083e7a992861f93f328a
Artem Basalaev <basa62.1996@gmail.com>
2024-05-23 00:26:47 +0200

    Spawning: non-shared state

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,39 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
-    // The `Connection` lets us read/write redis **frames** instead of
-    // byte streams. The `Connection` type is defined by mini-redis.
+    use std::collections::HashMap;
+
+    use mini_redis::Command::{self, Get, Set};
+
+    let mut db = HashMap::new();
+
+    // Connection, provided by `mini-redis`, handles parsing frames from the socket
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
+    // Use `read_frame` to receive a command from the connection.
+    while let Some(frame) = connection.read_frame().await.unwrap() {
         println!("GOT: {:?}", frame);
 
-        // Respond with an error
-        let response = Frame::Error("unimplemented".to_string());
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                // The value is stored as `Vec<u8>`
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            },
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    // `Frame::Bulk` expects data to be of type `Bytes`. This
+                    // type will be covered later in the tutorial. For now,
+                    // `&Vec<u8>` is converted to `Bytes` using `into()`.
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            },
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        // Write the response to the client
         connection.write_frame(&response).await.unwrap();
     }
 }
